64ec73c1c9fd16d1ada7f0355a3ec9a43061f44c
Mayank <apru.takkar@gmail.com>
2018-06-01 12:05:55 +0530

    Added RecycleView , Adapters and Retrofit API Hit

---
+++ app/src/main/java/com/ashima/android/Adapters/FeedAdapter.java
@@ -0,0 +1,79 @@
+package com.ashima.android.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ashima.android.Models.RowDataFeed;
+import com.ashima.android.R;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
+
+    private ArrayList<RowDataFeed> rowDataFeedList;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+    private Context mContext;
+
+    // data is passed into the constructor
+    public FeedAdapter(Context context, ArrayList<RowDataFeed> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.rowDataFeedList = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.row_feed, parent, false);
+        mContext = view.getContext();
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final RowDataFeed rowDataFeed = rowDataFeedList.get(position);
+        holder.tvTitle.setText(rowDataFeed.getTitle());
+        holder.tvDesc.setText(rowDataFeed.getDescription());
+        String imageURL = rowDataFeed.getImageHref();
+        if (imageURL != null)
+            Glide.with(mContext).load(imageURL).into(holder.ivThumbnail);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return rowDataFeedList.size();
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(RowDataFeed tblSfFormModel, int clickType);
+    }
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle, tvDesc;
+        ImageView ivThumbnail;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvDesc = itemView.findViewById(R.id.tvDescription);
+            ivThumbnail = itemView.findViewById(R.id.ivThumbnail);
+        }
+
+    }
+}
--- app/src/main/java/com/ashima/android/MainActivity.java
+++ app/src/main/java/com/ashima/android/MainActivity.java
@@ -1,13 +1,119 @@
 package com.ashima.android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.ashima.android.Adapters.FeedAdapter;
+import com.ashima.android.Models.FeedModel;
+import com.ashima.android.Models.RowDataFeed;
+import com.ashima.android.Utils.ApiInterface;
+import com.ashima.android.Utils.RetrofitApiClient;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private Toolbar toolbar;
+    private RecyclerView recyclerViewFeeds;
+    private FeedAdapter feedAdapter;
+    private ArrayList<RowDataFeed> rowDataFeedList;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    private void initView() {
+        // initialize all views by id
+        toolbar = findViewById(R.id.toolbar);
+        recyclerViewFeeds = findViewById(R.id.recyclerViewFeeds);
+        //add a ToolBar to ActionBar
+        setSupportActionBar(toolbar);
+    }
+
+
+    private void getFeedFromServer() {
+        // not using progress dialog as specification says not block UI while loading json
+        //showProgressDialogWithMsg("Please Wait While Receiving Feeds....");
+        ApiInterface apiService =
+                RetrofitApiClient.getClient().create(ApiInterface.class);
+
+        Call<FeedModel> call = apiService.getFeeds();
+        call.enqueue(new Callback<FeedModel>() {
+            @Override
+            public void onResponse(Call<FeedModel> call, Response<FeedModel> response) {
+                Log.d(TAG, "Success Feed GET");
+                if (response != null && response.isSuccessful()) {
+
+                    if (rowDataFeedList != null)
+                        rowDataFeedList.clear();
+                    else
+                        rowDataFeedList = new ArrayList<>();
+
+                    setToolbarTitle(response.body().getTitle());
+                    rowDataFeedList.addAll(response.body().getRows());
+                    setRecyclerViewAdapter();
+                    //dismissProgressDialogWithMsg();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<FeedModel> call, Throwable t) {
+                //dismissProgressDialogWithMsg();
+            }
+        });
+    }
+
+
+    private void setRecyclerViewAdapter() {
+        if (rowDataFeedList != null) {
+            if (feedAdapter == null) {
+                recyclerViewFeeds.setLayoutManager(new LinearLayoutManager(this));
+                DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerViewFeeds.getContext(),
+                        LinearLayoutManager.VERTICAL);
+                recyclerViewFeeds.addItemDecoration(dividerItemDecoration);
+                feedAdapter = new FeedAdapter(this, rowDataFeedList);
+                recyclerViewFeeds.setAdapter(feedAdapter);
+            } else {
+                feedAdapter.notifyDataSetChanged();
+            }
+        }
+
+    }
+
+    private void setToolbarTitle(String title) {
+        if (title != null&&toolbar!=null)
+            toolbar.setTitle(title);
+    }
+
+
+    public void showProgressDialogWithMsg(String msg) {
+        if (mProgressDialog == null)
+            mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setMessage(msg);
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        mProgressDialog.show();
+    }
+
+    public void dismissProgressDialogWithMsg() {
+        if (mProgressDialog != null && mProgressDialog.isShowing())
+            mProgressDialog.dismiss();
+    }
+
+
 }
